88016a2903591317342fd47219478619d8607faf
pruthviraj.urankar <pruthvirajurankar@yahoo.in>
2020-04-02 21:23:21 +0530

    Home Screen Added

--- app/src/main/java/com/example/moneymanager/model/Users.java
+++ app/src/main/java/com/example/moneymanager/model/Users.java
@@ -1,4 +1,40 @@
 package com.example.moneymanager.model;
 
 public class Users {
+    private  String email;
+    private  String name;
+    private  String username;
+    private  String phone_number;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
 }
